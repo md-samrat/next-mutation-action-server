edb2280263e1133d3a477d29cf72302707ca19b1
samrat-xyz <md.samrat61790@gmail.com>
2026-03-15 17:55:48 +0600

    implement action functionality for create user

--- src/lib/action/action.ts
+++ src/lib/action/action.ts
@@ -1,6 +1,21 @@
-"use server"
-export const createUser = async(formData:FormData) =>{
-    const name = formData.get("name");
-    const email = formData.get("email");
-    console.log("Name : ",name,"Email :",email)
-}
+"use server";
+
+import { dbConnect } from "../Database";
+import UserModel from "../models/user";
+
+export const createUser = async (formData: FormData) => {
+  await dbConnect();
+  const name = formData.get("name") as string;
+  const email = formData.get("email") as string;
+  try {
+    const newUser = new UserModel({ name, email });
+    await newUser.save();
+    return {
+      success: true,
+      message: "user create successfull",
+      user: JSON.parse(JSON.stringify(newUser)),
+    };
+  } catch (error) {
+    return { success: false, message: "failed to create user", error };
+  }
+};
